6bc05e16d15a6b54789aeb7977e60a0a4ee9d98d
vedanshgarg <vedansh.garg.7@gmail.com>
2018-07-09 11:40:28 +0530

    fix specialization padding

--- Static/Scripts/script.js
+++ Static/Scripts/script.js
@@ -16,15 +16,21 @@ var j=0;
  		var dist=document.querySelector(getId);
 		Ydist=dist.getBoundingClientRect().y;
  		console.log(Ydist);
+ 		var y=scrollY;
+ 		var x=scrollY;
+ 		var i=0;
 
- 		// while(scrollY<Ydist){
+ 		// while(y<Ydist){
  		// 	setTimeout(function(){
- 		// 		scrollTo(0,scrollY+15);
- 		// 	},1);
+ 		// 		x+=15;
+ 		// 		scrollTo(0,x);
+ 		// 	},i*5);
+ 		// 	y+=15;
+ 		// 	i+=1;
  		// }
 
  		y=setInterval(function(){
- 			if(scrollY+20>Ydist){
+ 			if(scrollY>Ydist){
  				clearTimeout(y);
  			}
  			if ((window.innerHeight + window.scrollY + 20) >= document.body.offsetHeight) {
